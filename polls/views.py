0159120b57bdb78acb00caadca4fe19747b2e7e8
dimasciput <dimas.ciputra@gmail.com>
2017-10-19 13:59:08 -0500

    Show map in index

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
+from django.core.serializers import serialize
 
 from .models import MapImage
 from .forms import MapImageForm
@@ -13,6 +14,11 @@ from .forms import MapImageForm
 class IndexView(ListView):
 	template_name = 'index.html'
 	context_object_name = 'all_map_image'
+
+	def get_context_data(self, **kwargs):
+		context = super(IndexView, self).get_context_data(**kwargs)
+		context['geojson'] = serialize('geojson', MapImage.objects.all(), geometry_field='location', fields=('name', 'image'))
+		return context
 
 	def get_queryset(self):
 		return MapImage.objects.all()
